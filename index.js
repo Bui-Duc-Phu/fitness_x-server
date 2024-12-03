eb2d89b6db340b5ecbb4b6752bd8533e3be7793a
longtq <tlong4856@gmail.com>
2024-12-03 14:29:33 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose")
+var bodyParser = require("body-parser")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+
+dotenv.config(); 
+
+app.use(bodyParser.json({limit: "50mb"}));
+
+app.use(cors());
+
+app.use(morgan("common"));
+
+//Connect Database
+mongoose.connect(process.env.MONGOODB_URL, {
+    dbName: 'fitness_x'
+})
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); 
+    });
+
+
+//Route
+app.use("/api/user", userRoute)
+
+app.listen(8000, () => {
+    console.log("Server is running ...")
+});
